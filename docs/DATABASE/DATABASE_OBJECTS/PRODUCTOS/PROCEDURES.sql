e96328f3d3eb46728bb5bb6b252fabcc373e9ebf
DhilsenDevs <160343685+Dhilsen18@users.noreply.github.com>
2025-12-01 16:14:46 -0500

    nuevo_procedimiento

--- docs/DATABASE/DATABASE_OBJECTS/PRODUCTOS/PROCEDURES.sql
+++ docs/DATABASE/DATABASE_OBJECTS/PRODUCTOS/PROCEDURES.sql
@@ -1,18 +1,27 @@
-#################################
--- PROCEDIMIENTO PRODUCTOS 2026 
-#################################
-DROP PROCEDURE IF EXISTS extraccion_productos_2026;
-DELIMITER $$;
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS extraccion_productos_2026 $$
+
 CREATE PROCEDURE extraccion_productos_2026(IN METHOD VARCHAR(50))
 BEGIN
-    IF METHOD = "LISTADO_NORMAL" THEN
-		SELECT NOMBRE, CODIGO FROM productos WHERE ESTADO = 1 ORDER BY NOMBRE;
-	ELSEIF METHOD = "LISTADO_INVENTARIO" THEN
-		SELECT ID, NOMBRE, CODIGO, UNIDAD_MEDIDA FROM productos where ESTADO = 1 ORDER BY NOMBRE;
-	END IF;
-END;
+    IF METHOD = 'LISTADO_NORMAL' THEN
+        SELECT NOMBRE, CODIGO 
+        FROM productos 
+        WHERE ESTADO = 1 
+        ORDER BY NOMBRE;
+
+    ELSEIF METHOD = 'LISTADO_INVENTARIO' THEN
+        SELECT ID, NOMBRE, CODIGO, UNIDAD_MEDIDA 
+        FROM productos 
+        WHERE ESTADO = 1 
+        ORDER BY NOMBRE;
+    END IF;
+END $$
+
 DELIMITER ;
 CALL extraccion_productos_2026('LISTADO_NORMAL');
 
 -- xd
 -- tundique
+
+use Zeus_Safety_Data_Integration;
