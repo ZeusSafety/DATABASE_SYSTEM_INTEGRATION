9329c80db06880c3b63a62f5a3037be34b4c8084
DhilsenDevs <160343685+Dhilsen18@users.noreply.github.com>
2025-12-01 16:03:00 -0500

    carpeta_produtos

---
+++ docs/DATABASE/DATABASE_OBJECTS/PRODUCTOS/PROCEDURES.sql
@@ -0,0 +1,18 @@
+#################################
+-- PROCEDIMIENTO PRODUCTOS 2026 
+#################################
+DROP PROCEDURE IF EXISTS extraccion_productos_2026;
+DELIMITER $$;
+CREATE PROCEDURE extraccion_productos_2026(IN METHOD VARCHAR(50))
+BEGIN
+    IF METHOD = "LISTADO_NORMAL" THEN
+		SELECT NOMBRE, CODIGO FROM productos WHERE ESTADO = 1 ORDER BY NOMBRE;
+	ELSEIF METHOD = "LISTADO_INVENTARIO" THEN
+		SELECT ID, NOMBRE, CODIGO, UNIDAD_MEDIDA FROM productos where ESTADO = 1 ORDER BY NOMBRE;
+	END IF;
+END;
+DELIMITER ;
+CALL extraccion_productos_2026('LISTADO_NORMAL');
+
+-- xd
+-- tundique
